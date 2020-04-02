d591153c617ce552004d98fa6debe0c05f65b5ce
miscalencu <george.miscalencu@gmail.com>
2020-04-02 13:22:59 +0200

    Remove Docs. Moved website to textgate.net

--- docs/static/js/5.10664e8b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpstats-app"]=this["webpackJsonpstats-app"]||[]).push([[5],{709:function(p,s,t){}}]);
-//# sourceMappingURL=5.10664e8b.chunk.js.map
--- docs/static/js/6.190c8a56.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpstats-app"]=this["webpackJsonpstats-app"]||[]).push([[6],{710:function(p,s,t){}}]);
-//# sourceMappingURL=6.190c8a56.chunk.js.map
